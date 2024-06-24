5b2b5c969bc101d74b6d51a2f839ddc95dea7ad8
Nicholas Kipkoech <nickey968@gmail.com>
2024-06-24 10:02:09 +0300

    add util function for fetching db keys

--- src/database/database.ts
+++ src/database/database.ts
@@ -1,10 +1,29 @@
 import oracledb from "oracledb";
 
+/** utility function for getting connection keys based on the enviroment */
+function getDbkeys() {
+  let user;
+  let password;
+  let connString;
+
+  if (process.env.ENVIROMENT === "MAYFAIR_TEST") {
+    user = process.env.MAYFAIR_DATABASE_USER;
+    password = process.env.MAYFAIR_DATABASE_PASSWORD;
+    connString = process.env.MAYFAIR_DATABASE_CONN_STRING;
+  } else if (process.env.ENVIROMENT === "INTRA") {
+    user = process.env.INTRA_DATABASE_USER;
+    password = process.env.INTRA_DATABASE_PASSWORD;
+    connString = process.env.INTRA_DATABASE_CONN_STRING;
+  }
+  return { user, password, connString };
+}
+
+const { user, password, connString } = getDbkeys();
 // Create and export the connection pool
 export const pool = oracledb.createPool({
-  user: "ICON",
-  password: "IC0N",
-  connectionString: "192.168.1.112:1521/BIMA19C",
+  user: user,
+  password: password,
+  connectionString: connString,
   poolMin: 5,
   poolMax: 100,
   poolIncrement: 5,
